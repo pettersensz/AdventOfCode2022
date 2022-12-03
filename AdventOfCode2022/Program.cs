3ef9ac140089bda66a4aacaa7936df430524b16b
SZP <pettersen.sigmund@gmail.com>
2022-12-03 18:28:00 +0100

    Day 3 Part 1 Solved

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -3,36 +3,46 @@ using AdventOfCode2022.Week01;
 
 namespace AdventOfCode2022
 {
-  internal class Program
-  {
-    static void Main(string[] args)
+    internal class Program
     {
-      var day1TestInput = ReadFile.ReadLinesInTextFile("Day01_example.txt");
-      var day1Test = new Day01(day1TestInput);
-      var day1TestResult = day1Test.GetElfWithMostCaloriesResult();
-      Console.WriteLine("Day 01 Test 1: " + day1TestResult);
-      var day1Part1Input = ReadFile.ReadLinesInTextFile("Day01_part1.txt");
-      var day1Part1 = new Day01(day1Part1Input);
-      var day1Part1Result = day1Part1.GetElfWithMostCaloriesResult();
-      Console.WriteLine("Day 01 Part 1: " + day1Part1Result);
-      var day1TestResult2 = day1Test.GetTopThreeElvesResult();
-      Console.WriteLine("Day 01 Test 2: " + day1TestResult2);
-      var day1Part2Result = day1Part1.GetTopThreeElvesResult();
-      Console.WriteLine("Day 01 Part 1: " + day1Part2Result);
-      Console.WriteLine("---- ---- ---- ---- ---- ---- ---- ----");
+        static void Main(string[] args)
+        {
+            var day1TestInput = ReadFile.ReadLinesInTextFile("Day01_example.txt");
+            var day1Test = new Day01(day1TestInput);
+            var day1TestResult = day1Test.GetElfWithMostCaloriesResult();
+            Console.WriteLine("Day 01 Test 1: " + day1TestResult);
+            var day1Part1Input = ReadFile.ReadLinesInTextFile("Day01_part1.txt");
+            var day1Part1 = new Day01(day1Part1Input);
+            var day1Part1Result = day1Part1.GetElfWithMostCaloriesResult();
+            Console.WriteLine("Day 01 Part 1: " + day1Part1Result);
+            var day1TestResult2 = day1Test.GetTopThreeElvesResult();
+            Console.WriteLine("Day 01 Test 2: " + day1TestResult2);
+            var day1Part2Result = day1Part1.GetTopThreeElvesResult();
+            Console.WriteLine("Day 01 Part 1: " + day1Part2Result);
+            Console.WriteLine("---- ---- ---- ---- ---- ---- ---- ----");
 
-      var day2TestInput = ReadFile.ReadLinesInTextFile("Day02_example.txt");
-      var day2Test = new Day02(day2TestInput);
-      var day2TestPart1 = day2Test.GetTotalScore();
-      Console.WriteLine("Day 02 Test 1: " + day2TestPart1);
-      var day2Input = ReadFile.ReadLinesInTextFile("Day02_large.txt");
-      var day2 = new Day02(day2Input);
-      var day2Part1 = day2.GetTotalScore();
-      Console.WriteLine("Day 02 Part 1: " + day2Part1);
-      var day2TestPart2 = day2Test.GetTotalScoreWithDesiredOutcome();
-      Console.WriteLine("Day 02 Test 2: " + day2TestPart2);
-      var day2Part2 = day2.GetTotalScoreWithDesiredOutcome();
-      Console.WriteLine("Day 02 Part 2: " + day2Part2);
+            var day2TestInput = ReadFile.ReadLinesInTextFile("Day02_example.txt");
+            var day2Test = new Day02(day2TestInput);
+            var day2TestPart1 = day2Test.GetTotalScore();
+            Console.WriteLine("Day 02 Test 1: " + day2TestPart1);
+            var day2Input = ReadFile.ReadLinesInTextFile("Day02_large.txt");
+            var day2 = new Day02(day2Input);
+            var day2Part1 = day2.GetTotalScore();
+            Console.WriteLine("Day 02 Part 1: " + day2Part1);
+            var day2TestPart2 = day2Test.GetTotalScoreWithDesiredOutcome();
+            Console.WriteLine("Day 02 Test 2: " + day2TestPart2);
+            var day2Part2 = day2.GetTotalScoreWithDesiredOutcome();
+            Console.WriteLine("Day 02 Part 2: " + day2Part2);
+            Console.WriteLine("---- ---- ---- ---- ---- ---- ---- ----");
+
+            var day3TestInput = ReadFile.ReadLinesInTextFile("Day03_example.txt");
+            var day3Test = new Day03(day3TestInput);
+            var day3TestPart1 = day3Test.GetPriorities();
+            Console.WriteLine("Day 03 Test 1: " + day3TestPart1);
+            var day3Input = ReadFile.ReadLinesInTextFile("Day03_large.txt");
+            var day3 = new Day03(day3Input);
+            var day3Part1 = day3.GetPriorities();
+            Console.WriteLine("Day 03 Part 1: " + day3Part1);
+        }
     }
-  }
 }
---
+++ AdventOfCode2022/Week01/Day03.cs
@@ -0,0 +1,59 @@
+﻿namespace AdventOfCode2022.Week01
+{
+ internal class Day03
+ {
+  public List<Rucksack> Rucksacks { get; set; } = new();
+  public Day03(string[] input)
+  {
+   foreach (var line in input)
+   {
+    var rucksack = new Rucksack();
+    //Console.Write(line.Length + "-");
+    var part1 = line.Substring(0, line.Length / 2);
+    rucksack.Compartment1 = part1.ToCharArray();
+    //Console.Write(part1.Length + "-");
+    var part2 = line.Substring(line.Length / 2);
+    rucksack.Compartment2 = part2.ToCharArray();
+    //Console.Write(part2.Length + "\n");
+    Rucksacks.Add(rucksack);
+   }
+  }
+
+  internal class Rucksack
+  {
+   public char[]? Compartment1 { get; set; }
+   public char[]? Compartment2 { get; set; }
+  }
+
+  internal int GetPriorities()
+  {
+   var prioritiesSum = 0;
+   foreach (var rucksack in Rucksacks)
+   {
+    char commonChar = FindCommonChar(rucksack);
+    //Console.Write(commonChar + " - ");
+    var priority = GetPriority(commonChar);
+    prioritiesSum += priority;
+   }
+   return prioritiesSum;
+  }
+
+  private int GetPriority(char character)
+  {
+   var priority = character % 32;
+   if (Char.IsUpper(character)) priority += 26;
+   //Console.Write(priority + "\n");
+   return priority;
+  }
+
+  private char FindCommonChar(Rucksack rucksack)
+  {
+   if (rucksack.Compartment1 != null && rucksack.Compartment2 != null)
+   {
+    var commonChar = rucksack.Compartment1.Where(c => rucksack.Compartment2.Contains(c)).First();
+    return commonChar;
+   }
+   throw (new InvalidOperationException("Complartments not initialized"));
+  }
+ }
+}
